4df77a2c84bd2a4a5ad2d9a648cbcabc85816fe0
Matias Poblete <86752543+MattPoblete@users.noreply.github.com>
2024-10-12 21:44:56 +0100

    ✨Created connect wallet modal

--- src/components/web3/ConnectButton.tsx
+++ src/components/web3/ConnectButton.tsx
@@ -1,106 +1,26 @@
-import 'twin.macro'
-import {useSorobanReact} from "@soroban-react/core"
-
-import {
-  Button,
-  HStack,
-  Menu,
-  MenuButton,
-  MenuDivider,
-  MenuItem,
-  MenuList,
-  Text,
-  VStack,
-} from '@chakra-ui/react'
-import { FiChevronDown } from 'react-icons/fi'
-import { AiOutlineCheckCircle, AiOutlineDisconnect } from 'react-icons/ai'
-import toast from 'react-hot-toast'
-import type { WalletChain } from '@soroban-react/types'
+import React from "react"
+import { Button } from '@chakra-ui/react'
+import { useSorobanReact } from '@soroban-react/core'
+import { ConnectWalletModal } from './ConnectWalletModal'
+import { useState } from 'react'
 
 export const ConnectButton = () => {
-    // Connect Button
-    const sorobanContext = useSorobanReact()
-
-    const {activeChain, address, disconnect, setActiveConnectorAndConnect, setActiveChain} = sorobanContext;
-    const activeAccount = address;
-
-  const browserWallets = sorobanContext.connectors;
-
-    if (!activeAccount)
-      return (
-        <Menu>
-          <MenuButton
-            as={Button}
-            size="md"
-            rightIcon={<FiChevronDown size={22} />}
-            py={6}
-            fontWeight="bold"
-            rounded="2xl"
-            colorScheme="purple"
-          >
-            Connect Wallet
-          </MenuButton>
-
-          <MenuList bgColor="blackAlpha.900" borderColor="whiteAlpha.300" rounded="2xl">
-            {/* Installed Wallets */}
-            {!activeAccount &&
-              browserWallets.map((w) => 
-                  <MenuItem
-                    key={w.name}
-                    onClick={() => {
-                      setActiveConnectorAndConnect && setActiveConnectorAndConnect(w)
-                    }}
-                    tw="bg-transparent hocus:bg-gray-800"
-                  >
-                    {w.name}
-                  </MenuItem>
-              )}
-          </MenuList>
-        </Menu>
-      )
-      
-
-    // Account Menu & Disconnect Button
-    return (
-      <Menu matchWidth>
-        <HStack>
-          {/* Account Name, Address, and AZNS-Domain (if assigned) */}
-          <MenuButton
-            as={Button}
-            rightIcon={<FiChevronDown size={22} />}
-            hidden={false}
-            py={6}
-            pl={5}
-            rounded="2xl"
-            fontWeight="bold"
-          >
-            <VStack spacing={0.5}>
-              {/* <AccountName account={activeAccount} /> */}
-              <Text>{activeChain?.name}</Text>
-              <Text fontSize="xs" fontWeight="normal" opacity={0.75}>
-                {address}
-              </Text>
-            </VStack>
-          </MenuButton>
-        </HStack>
-
-        <MenuList
-          bgColor="blackAlpha.900"
-          borderColor="whiteAlpha.300"
-          rounded="2xl"
-          maxHeight="40vh"
-        >
-
-          {/* Disconnect Button */}
-          <MenuDivider />
-          <MenuItem
-            onClick={async () => {console.log("Disconnecting"); await disconnect()}}
-            icon={<AiOutlineDisconnect size={18} />}
-            tw="bg-transparent hocus:bg-gray-800"
-          >
-            Disconnect
-          </MenuItem>
-        </MenuList>
-      </Menu>
-    )
+  const { address } = useSorobanReact()
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false)
+  const handleClick = () => {
+    setIsModalOpen(true)
   }
+  const handleClose = () => {
+    setIsModalOpen(false)
+  }
+  return (
+    <>
+      <ConnectWalletModal isOpen={isModalOpen} onClose={handleClose} />
+      <Button sx={{ mx: 4, px: 6 }} colorScheme='green' onClick={handleClick} rounded={18} mb={{ base: 4, md: 0 }}>
+        {address ? 'Disconnect' : 'Connect'}
+      </Button>
+    </>
+  )
+}
+
+export default ConnectButton
