9a531f1f423ed3610d7882dd043ee31b2cd54a7e
esteblock <estebaniglesiasmanriquez@gmail.com>
2024-10-12 13:03:06 -0300

    initialized function tested

--- contracts/payroll-vault/src/test/initialize.rs
+++ contracts/payroll-vault/src/test/initialize.rs
@@ -1,7 +1,19 @@
- 
+use crate::test::PaymentVaultTest;
+use crate::test::payroll_vault::ContractError;
+
 #[test]
 fn test_initialize() {
-    // let test = SoroswapAggregatorTest::setup();
-    todo!()
+    let test = PaymentVaultTest::setup();
+
+    // Initialize with token address
+    test.contract.initialize(&test.token.address);
+    
+    // Check that the token address is set
+    assert_eq!(test.contract.asset(), test.token.address);
+
+    // Cannot initialize again
+
+    let result = test.contract.try_initialize(&test.token.address);
+    assert_eq!(result, Err(Ok(ContractError::AlreadyInitialized)));
 }
  
