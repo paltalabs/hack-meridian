973e2db36bcfdb30e6b12596deb48094e218fbcb
Matias Poblete <86752543+MattPoblete@users.noreply.github.com>
2024-10-13 01:19:45 +0100

    ✨Add balance section

--- src/components/layout/MainPage.tsx
+++ src/components/layout/MainPage.tsx
@@ -3,18 +3,21 @@ import React, { useState } from 'react'
 import { ProfileDrawer } from '../Drawer/Drawer'
 import { SearchBar } from './SearchBar'
 import { TradContractsAccordion } from '../Accordion/TradContractsAccorrdion'
+import { Stack, Text } from '@chakra-ui/react'
 
 export const MainPage = () => {
   const { address } = useSorobanReact()
   const [isDrawerOpen, setIsDrawerOpen] = useState<boolean>(false)
-  const contracts = ['GC2VCACI4VCD3RQF3JNQNYYCGXAMZRQ3LKF3Q5425W7RIY6PR2UUNNEB', 'GC2VCACI4VCD3RQF3JNQNYYCGXAMZRQ3LKF3Q5425W7RIY6PR2UUNNEB', 'GC2VCACI4VCD3RQF3JNQNYYCGXAMZRQ3LKF3Q5425W7RIY6PR2UUNNEB']
 
   if (!address) return null;
   return (
     <>
       <ProfileDrawer isOpen={isDrawerOpen} onClose={() => { setIsDrawerOpen(false) }} />
       <SearchBar handleOpenDrawer={setIsDrawerOpen} />
-
+      <Stack>
+        <Text>Business account balance:</Text>
+        <Text as={'b'} fontSize={'3xl'}>$ 1000</Text>
+      </Stack>
       <TradContractsAccordion />
     </>
   )
