c90dd41bf9729aba0a6ed9d9ab09542e2f720df7
coderipper <hello@joaquinsoza.dev>
2024-10-13 15:37:22 +0100

    removed hana wallet

--- src/components/web3/MySorobanReactProvider.tsx
+++ src/components/web3/MySorobanReactProvider.tsx
@@ -3,13 +3,11 @@ import { SorobanReactProvider } from '@soroban-react/core';
 import { futurenet, sandbox, standalone, testnet } from '@soroban-react/chains';
 import { freighter } from '@soroban-react/freighter';
 import type { ChainMetadata, Connector } from "@soroban-react/types";
-import { lobstr } from '@soroban-react/lobstr';
-import { hana } from '@soroban-react/hana';
 
 import deployments from '../../../contracts/deployments.json';
 
 const chains: ChainMetadata[] = [sandbox, standalone, futurenet, testnet];
-export const connectors: Connector[] = [freighter(), hana()];
+export const connectors: Connector[] = [freighter()];
 
 
 export default function MySorobanReactProvider({ children }: { children: React.ReactNode }) {
