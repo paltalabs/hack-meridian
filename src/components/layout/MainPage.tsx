06d480b64fa75d3c4ac10ad9cb1718a116d62b09
Matias Poblete <86752543+MattPoblete@users.noreply.github.com>
2024-10-13 01:12:50 +0100

    ✨Add accordion component

--- src/components/layout/MainPage.tsx
+++ src/components/layout/MainPage.tsx
@@ -1,19 +1,24 @@
 import { useSorobanReact } from '@soroban-react/core'
 import React, { useState } from 'react'
 import { ProfileDrawer } from '../Drawer/Drawer'
-import { Avatar, Grid, GridItem, IconButton, Input, InputGroup, InputLeftElement, InputRightElement } from '@chakra-ui/react'
+import { Accordion, AccordionButton, AccordionItem, AccordionPanel, Avatar, Button, Card, CardBody, CardFooter, Grid, GridItem, Heading, IconButton, Input, InputGroup, InputLeftElement, InputRightElement, Stack, Text } from '@chakra-ui/react'
 import { AddIcon, Search2Icon } from '@chakra-ui/icons'
+import { BsThreeDotsVertical, BsTrash } from "react-icons/bs";
 import { SearchBar } from './SearchBar'
+import { shortenAddress } from '@/utils/shortenAdress'
+import { TradContractsAccordion } from '../Accordion/TradContractsAccorrdion'
 
 export const MainPage = () => {
   const { address } = useSorobanReact()
   const [isDrawerOpen, setIsDrawerOpen] = useState<boolean>(false)
+  const contracts = ['GC2VCACI4VCD3RQF3JNQNYYCGXAMZRQ3LKF3Q5425W7RIY6PR2UUNNEB', 'GC2VCACI4VCD3RQF3JNQNYYCGXAMZRQ3LKF3Q5425W7RIY6PR2UUNNEB', 'GC2VCACI4VCD3RQF3JNQNYYCGXAMZRQ3LKF3Q5425W7RIY6PR2UUNNEB']
 
   if (!address) return null;
   return (
     <>
-      <SearchBar handleOpenDrawer={setIsDrawerOpen} />
       <ProfileDrawer isOpen={isDrawerOpen} onClose={() => { setIsDrawerOpen(false) }} />
+      <SearchBar handleOpenDrawer={setIsDrawerOpen} />
+      <TradContractsAccordion />
     </>
   )
 }
