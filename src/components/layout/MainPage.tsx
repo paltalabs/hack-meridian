53dbf97ac69d86356c0d13a11d13226107c8c07f
Matias Poblete <86752543+MattPoblete@users.noreply.github.com>
2024-10-13 01:49:32 +0100

    ✨Add create contract modal

--- src/components/layout/MainPage.tsx
+++ src/components/layout/MainPage.tsx
@@ -3,17 +3,21 @@ import React, { useState } from 'react'
 import { ProfileDrawer } from '../Drawer/Drawer'
 import { SearchBar } from './SearchBar'
 import { TradContractsAccordion } from '../Accordion/TradContractsAccorrdion'
-import { Stack, Text } from '@chakra-ui/react'
+import { Avatar, Button, Input, InputGroup, InputLeftElement, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, NumberDecrementStepper, NumberIncrementStepper, NumberInput, NumberInputField, NumberInputStepper, Select, Stack, Text } from '@chakra-ui/react'
+import { AddIcon } from '@chakra-ui/icons'
+import { CreateContractModal } from '../Modals/CreateContractModal'
 
 export const MainPage = () => {
   const { address } = useSorobanReact()
   const [isDrawerOpen, setIsDrawerOpen] = useState<boolean>(false)
+  const [isCreateContractModalOpen, setIsCreateContractModalOpen] = useState<boolean>(true) 
 
   if (!address) return null;
   return (
     <>
       <ProfileDrawer isOpen={isDrawerOpen} onClose={() => { setIsDrawerOpen(false) }} />
-      <SearchBar handleOpenDrawer={setIsDrawerOpen} />
+      <SearchBar handleOpenDrawer={setIsDrawerOpen} handleCreateContract={setIsCreateContractModalOpen} />
+      <CreateContractModal isOpen={isCreateContractModalOpen} onClose={setIsCreateContractModalOpen} />
       <Stack>
         <Text>Business account balance:</Text>
         <Text as={'b'} fontSize={'3xl'}>$ 1000</Text>
